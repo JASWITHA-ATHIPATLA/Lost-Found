df57f9aa0690826ab2a0699707edae218bd4b7ed
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2026-02-09 17:01:04 +0530

    Lost&Found

---
+++ src/components/ShareSection.jsx
@@ -0,0 +1,26 @@
+// import React from "react";
+// import ItemCards from "./ItemCard.jsx";
+// import styled from "styled-components";
+
+// function ShareSection() {
+//    function shareTo() {
+//   console.log("Share clicked"); // 👀 must appear
+
+//   const pageUrl = window.location.href;
+//   const shareText = "Help Us To Find Items";
+
+//   const whatsappAPI =
+//     "https://wa.me/?text=" +
+//     encodeURIComponent(`${shareText} ${pageUrl}`);
+
+//   window.open(whatsappAPI, "_blank");
+// }
+
+//   return (
+//     <>
+//       <ItemCards shareTo={shareTo} />
+//     </>
+//   );
+// }
+
+// export default ShareSection;
