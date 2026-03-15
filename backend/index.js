7235649de5b3661d8ed242b7f0496c3d58e4998a
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2026-03-15 23:08:59 +0530

    Lost&Found

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+const app=express();
+app.use(express.json());
+// let items = [
+//   { id: 1, name: "Amit" },
+//   { id: 2, name: "Rahul" }
+// ];
+
 app.get('/',(req,res)=>{
    res.send("hlo");
+})
+app.post('/newItemData',(req,res)=>{
+   
 })
 app.listen(3000,()=>{
  console.log(`server is running on port ${3000}`);
